82bfa9ae4d9aca18d0aaa25ae85eea088b88ffae
Konstantin_P <mail@mail.ru>
2025-09-11 18:23:00 +0300

    task "form to add todo item"

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@ import Task3_Likes from './task3/Task3_Likes.vue';
 import Task4_Forms from './task4/Task4_Forms.vue';
 
 let arr = reactive([
-   { id: 1, name: 'TodoList', component: Task1_TodoList, show: false },
+   { id: 1, name: 'TodoList', component: Task1_TodoList, show: true },
    { id: 2, name: 'ShoppingCart', component: Task2_ShoppingCart, show: false },
    { id: 3, name: 'Likes', component: Task3_Likes, show: false },
-   { id: 4, name: 'Forms', component: Task4_Forms, show: true },
+   { id: 4, name: 'Forms', component: Task4_Forms, show: false },
 ]);
 </script>
 
